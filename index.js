01738231f2824cdc2bc29570ead7878ea78fde1a
Waqas Ali <mwaqas.dc789@gmail.com>
2024-05-31 02:30:51 +0500

    xyz

--- index.js
+++ index.js
@@ -45,14 +45,16 @@ const chatMessage = require("./model/chatMessage")
 
   // end of automaticaaly chnage status
 //creating session
-const session = require("express-session");
-app.use(
-    session({
-      secret: process.env.SESSION_KEY,
-      resave: true,
-      saveUninitialized: true,
-    })
-  );
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+app.use(session({
+  secret: process.env.SESSION_KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000 } // 3 hours
+}));
 
 // path
 const staticPath = path.join(__dirname, "public")
